7ea9adffe9dfa6983500f0e45524f0e97832b130
gabrielmarcos2 <gabrielsqueda123@gmail.com>
2022-11-12 18:19:09 -0300

    tarefa finalizada

---
+++ index.js
@@ -0,0 +1,5 @@
+let nome = prompt("DIGITE O SEU NOME");
+let sobrenome = prompt("DIGITE SEU SOBRENOME");
+
+let nomecompleto = nome + sobrenome;
+alert(`seu nome completo é: ${nome + " " + sobrenome} \nSeu nome de catalogo é: ${sobrenome.toUpperCase()}, ${nome}`);
